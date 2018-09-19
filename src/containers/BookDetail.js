eef2700324fabeb4f3df3b1e8ff8170db54c1996
veselinn16 <veselinn16@gmail.com>
2018-09-19 18:24:05 +0300

    Add BookDetail container :wrench:

---
+++ src/containers/BookDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BookDetail extends Component {
+    render() {
+        return (
+            <div>BOOk Detail</div>
+        )
+    }
+}
+
+export default BookDetail;

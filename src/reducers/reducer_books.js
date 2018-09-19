94d70f54902627faf7fc35c3249879e8f76e9b9a
veselinn16 <veselinn16@gmail.com>
2018-09-19 14:39:15 +0300

    Add BooksReducer :construction:

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Perfume'},
+        {title: 'The Great Gatsby'},
+        {title: 'The Name of the Rose'},
+        {title: '1984'}
+    ]
+}
